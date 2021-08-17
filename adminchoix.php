a18f3d4c262e991d6ca1deb8260de65649defdf2
[ChronaMakenshi] <[christophe.mestdagh@sfr.fr]>
2021-08-17 10:12:51 +0200

    admin.php

---
+++ adminchoix.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style1.css">
+    <title>Connexion au Quizz World</title>
+</head>
+<body class="text-primary">
+    <div class="d-flex flex-wrap bd-highlight mb-3">
+        <h1 class="m-3 p-2 bd-highlight m-auto">Quizz World</h1>
+    </div>
+    <form class="p-2 bg-opacity">
+    <div class="text-center">
+        <h2 class="my-5">Admistration du Quizz World</h2>
+    </div>
+    <div class="table-responsive">
+    <table class="table  text-center m-auto table-striped table-primary">
+    <tr>
+        <td>ID</td>
+        <td>Titre</td>
+        <td>Nom de l'image</td>
+        <td>Supprimé</td>
+        <td>Validé</td>
+<?php
+
+{
+    $dsn = "mysql:dbname=bdd-quizz;host=localhost:3306";
+    try {
+        $option = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+        $connexion = new PDO($dsn, "root", "",$option);
+    } catch (PDOException $e) {
+        printf("Echec connexion : %s\n", $e->getMessage());
+        
+    }
+    
+}
+
+
+$reponse = $connexion->query("select * from inscription");
+
+;    
+    
+
+while($ligne = $reponse -> fetch() ){
+    $pseudo=$ligne['pseudo'];
+    $mail=$ligne['mail'];
+    $id=$ligne['id'];
+    $mail=$ligne['role'];
+    echo "<tr><td>".$ligne["id"]."</td>";
+    echo "<td>".$ligne["pseudo"]."</a></td>";
+    echo "<td>".$ligne["mail"]."</td>";
+    echo "<td><a href='supprimer.php?code= $ligne[id]'> Suprimer </a></td>";
+    echo "<td><a href='valide.php?code= $ligne[id]'>Valider</a></td></tr>";
+}
+?>
+    </tr>
+</table>
+
+</body>
+</html>
